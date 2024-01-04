387ff3a7d7cd383bf5a52a45ea19300ea8d26411
Ian Curl <iancurl.salvador21@gmail.com>
2024-01-04 21:42:07 +0800

    First commit ni boss
    Inasikaso ko yung login page

---
+++ Public/register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Page</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+        <div class="header">
+            <div class="business-title-shadow">Street Bites</div>
+            <div class="left-section">
+                <img src="../images/logo.jpg" class="logo">
+            </div>
+            <div class="right-section">
+                <div class="business-title">Street Bites</div>
+            </div>
+        </div>
+
+        <div class="main-content">
+            <div class="wrapper">
+
+
+
+            
+            </div>
+
+        </div>
+
+</body>
+</html>
---
+++ config/connection.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+
+define('HOST','localhost');
+define('USERNAME','root');
+define('PASSWORD','');
+define('DB_NAME', 'miryenda_management_system');
+
+$connection = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME);
+
+// if(mysqli_connect_error()){
+//     echo ("Sayang Boss, bawal eh");
+// }else{
+//     echo("Nuxx Wan brad! Pasok");
+// }
+
+?>
